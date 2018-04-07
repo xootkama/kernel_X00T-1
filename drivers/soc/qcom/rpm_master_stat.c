8eebac41b573c30b56627d960882fffcf4b6bc9c
Nathan Chancellor <natechancellor@gmail.com>
2018-04-06 23:43:51 -0700

    msm-4.4: Fix strlcpy-strlcat-size Clang warnings
    warning: size argument in 'strlcpy' call appears to be size of the source; expected the size of the destination [-Wstrlcpy-strlcat-size]
    
    Change-Id: Ide53f8546dcd0b8b2401899e889804b367c3f2d8
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Kendras056 <kendras056@gmail.com>

--- drivers/soc/qcom/rpm_master_stat.c
+++ drivers/soc/qcom/rpm_master_stat.c
@@ -397,7 +397,7 @@ static struct msm_rpm_master_stats_platform_data
 			goto err;
 		rpm_master_name_len = strlen(master_name);
 		strlcpy(pdata->masters[i], master_name,
-					rpm_master_name_len + 1);
+					strlen(pdata->masters[i]) + 1);
 	}
 	return pdata;
 err:
